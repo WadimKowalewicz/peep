7142b250a15f599be40cb3c992f8fab20de3381a
vadzim kavalevich <vadzim.kavalevich@gmail.com>
2023-08-15 14:35:08 +0200

    fix: styles modules

--- src/components/profile/myPosts/post/Post.jsx
+++ src/components/profile/myPosts/post/Post.jsx
@@ -6,8 +6,6 @@ import c from './Post.module.css'
 const Post = (props) => {
     return (
         <div className={c.post}>
-            {/*<img src="https://sm.ign.com/ign_ap/cover/a/avatar-gen/avatar-generations_hugw.jpg"/>*/}
-
                  <img src={props.ava}/>
                 {props.message}
             <div>
