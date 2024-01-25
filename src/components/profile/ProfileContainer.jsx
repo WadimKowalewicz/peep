e0cc038c4a83a983d53370920866c169d841c0fc
VadzimKavalevich <vadzim.kavalevich@gmail.com>
2024-01-25 22:50:32 +0100

    feat: user photo upload fix: shouldComponentUpdate

--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Profile from "./Profile";
-import {getStatus, getUserProfile, updateStatus} from "../../redux/profileReducer";
+import {getStatus, getUserProfile, updateStatus, savePhoto} from "../../redux/profileReducer";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {compose} from "redux";
@@ -8,7 +8,8 @@ import {compose} from "redux";
 
 
 class ProfileContainer extends React.Component {
-    componentDidMount() {
+
+    refreshProfile() {
         let userId = this.props.match.params.userId;
         if(!userId) {
             userId = this.props.authorizedUserId;
@@ -20,13 +21,25 @@ class ProfileContainer extends React.Component {
         this.props.getStatus(userId);
     }
 
+    componentDidMount() {
+        this.refreshProfile()
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId != prevProps.match.params.userId) {
+        this.refreshProfile()
+        }
+    }
+
     render()
     {
         return (
             <Profile {...this.props}
+                     isOwner={!this.props.match.params.userId}   
                      profile={this.props.profile}
                      status={this.props.status}
                      updateStatus={this.props.updateStatus}
+                     savePhoto={this.props.savePhoto}
             />
         )
     }
@@ -43,7 +56,7 @@ let mapStateToProps = (state) => {
 }
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto}),
     withRouter,
 //    withAuthRedirect
 )(ProfileContainer)
--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import c from './ProfileInfo.module.css'
 import Preloader from "../../common/preloader/Preloader";
-import ProfileStatus from "./ProfileStatus"
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from '../../../assets/images/userPhoto.png';
 
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
-    if(!profile) {
+const ProfileInfo = ({ profile, status, updateStatus, isOwner, savePhoto }) => {
+    if (!profile) {
         return <Preloader />
     }
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0])
+        }
+    }
+
     return (
         <div>
-            <img src='https://wallpapercave.com/wp/wp2461898.jpg'/>
-            <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             <div className={c.descriptionBlock}>
-                <img className={c.userPhoto} src={profile.photos.large} />
+                <img src='https://wallpapercave.com/wp/wp2461898.jpg' />
+                <img src={profile.photos.large || userPhoto} className={c.userPhoto} />
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
+               
                 <div>{profile.fullName}</div>
                 <div>{profile.aboutMe}</div>
                 <a href={profile.contacts.twitter} target={"_blank"}>X</a>
